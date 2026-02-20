1f9a0cd37e9a7867f0129037c41c265afebded05
Satoyoshi_ISHIZAKI <ishizaki-satoyoshi766@g.ecc.u-tokyo.ac.jp>
2026-02-20 14:03:00 +0900

    Finished taxonomic assignment on ITS data.

--- scripts/taxonomy_ITS.sh
+++ scripts/taxonomy_ITS.sh
@@ -1,5 +1,16 @@
 # assign taxonomy to ASVs
 
+## Workflow to this file:
+## 1. qiime2_install.sh
+## 2. make_manifest.sh
+## 3. import.sh
+## 4. trim.sh
+## 5. dada2_ITS.sh
+## 6. merge.sh
+## 7. filter_singltons.sh
+## 8. classifier_install.sh
+## 9. taxomony_ITS.sh (this file)
+
 ## start docker container first
 # docker start qiime2.(version)
 # docker exec -i -t qiime2.(version) /bin/bash
@@ -12,44 +23,54 @@
 # Abarenkov et al. (2024). The UNITE database for molecular identification and taxonomic communication of fungi and other eukaryotes: sequences, taxa and classifications reconsidered.
 # 10.15156/BIO/2959338
 
-# make dir
-# mkdir ../UNITE_all_20240404
-
 # use qiime2.(version) to use scikit-sklearn v 1
 # docker start qiime2.(version)
 # docker exec -i -t qiime2.(version) /bin/bash
 
-#import repseqs and taxonomy file
-qiime tools import \
-  --type 'FeatureData[Sequence]' \
-  --input-path /data/sh_qiime_release_all_04.04.2024/developer/sh_refs_qiime_ver10_dynamic_all_04.04.2024_dev.fasta \
-  --output-path /data/UNITE_all_20240404/unite_refseqs_240404.qza
+## previous code
 
-qiime tools import \
-  --type 'FeatureData[Taxonomy]' \
-  --input-format HeaderlessTSVTaxonomyFormat \
-  --input-path /data/sh_qiime_release_all_04.04.2024/developer/sh_taxonomy_qiime_ver10_dynamic_all_04.04.2024_dev.txt \
-  --output-path /data/UNITE_all_20240404/unite_taxonomy_240404.qza
+### make dir
+### mkdir ../UNITE_all_20240404
+
+### import repseqs and taxonomy file
+# qiime tools import \
+#   --type 'FeatureData[Sequence]' \
+#   --input-path /data/sh_qiime_release_all_04.04.2024/developer/sh_refs_qiime_ver10_dynamic_all_04.04.2024_dev.fasta \
+#   --output-path /data/UNITE_all_20240404/unite_refseqs_240404.qza
+
+# qiime tools import \
+#   --type 'FeatureData[Taxonomy]' \
+#   --input-format HeaderlessTSVTaxonomyFormat \
+#   --input-path /data/sh_qiime_release_all_04.04.2024/developer/sh_taxonomy_qiime_ver10_dynamic_all_04.04.2024_dev.txt \
+#   --output-path /data/UNITE_all_20240404/unite_taxonomy_240404.qza
 
 #train classifier
-qiime feature-classifier fit-classifier-naive-bayes \
-  --i-reference-reads /data/UNITE_all_20240404/unite_refseqs_240404.qza \
-  --i-reference-taxonomy /data/UNITE_all_20240404/unite_taxonomy_240404.qza \
-  --o-classifier /data/UNITE_all_20240404/unite_2404_nb_classifier.qza
+# qiime feature-classifier fit-classifier-naive-bayes \
+#   --i-reference-reads /data/UNITE_all_20240404/unite_refseqs_240404.qza \
+#   --i-reference-taxonomy /data/UNITE_all_20240404/unite_taxonomy_240404.qza \
+#   --o-classifier /data/UNITE_all_20240404/unite_2404_nb_classifier.qza
 
+## current code
 #assign taxonomy to ASVs
 qiime feature-classifier classify-sklearn \
-    --i-reads Analysis/denoised/rep-seqs_ITS.qza \
-    --i-classifier /data/UNITE_all_20240404/unite_2404_nb_classifier.qza \
-    --o-classification Analysis/taxa/classification_ITS.qza
+  --i-classifier classifiers/unite_ver2025-02-19_dynamic_eukaryotes-Q2-2026.1.qza \
+  --i-reads Analysis/merge/seq_ITS_ms2.qza \
+  --o-classification Analysis/taxa/classification_ITS.qza
 
-#barplot
+qiime feature-table tabulate-seqs \
+  --i-data Analysis/merge/seq_ITS_ms2.qza \
+  --i-taxonomy Analysis/taxa/classification_ITS.qza \
+  --m-metadata-file Analysis/merge/asv-frequencies_ITS_ms2.qza \
+  --o-visualization Analysis/taxa/classification_ITS.qzv
+
+# barplot
 qiime taxa barplot \
-    --i-table Analysis/denoised/table_ITS.qza \
-    --i-taxonomy Analysis/taxa/classification_ITS.qza \
-    --o-visualization Analysis/taxa/taxa_barplot_ITS.qzv
+  --i-table Analysis/merge/table_ITS_ms2.qza \
+  --i-taxonomy Analysis/taxa/classification_ITS.qza \
+  --o-visualization Analysis/taxa/barplots_ITS_ms2.qzv
 
-#filter out ASVs that were not assigned to the kingdom "Fungi"
+# Yet To be done
+# filter out ASVs that were not assigned to the kingdom "Fungi"
 qiime taxa filter-table \
     --i-table Analysis/denoised/table_ITS.qza \
     --i-taxonomy Analysis/taxa/classification_ITS.qza \
